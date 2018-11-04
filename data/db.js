0d8a6d69a6048fb79cb795e78892bc39e28418af
ryanboris <ryan.boris@gmail.com>
2018-11-04 01:37:09 -0500

    Initial Commit

--- data/db.js
+++ data/db.js
@@ -11,7 +11,7 @@ module.exports = {
 };
 
 function find() {
-  return db('users');
+  return db('users'); 
 }
 
 function findById(id) {
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+const express = require('express')
+const data = require('./data/db.js')
+
+const server = express()
+
+server.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee'
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins'
+        }
+    ]
+
+    res.status(200).json(hobbits)
+})
+
+server.get('/users', (req, res) => {
+    data.find().then(users => res.status(200).json(users)).catch(e => console.log(e, res.status(500), "error"))
+})
+
+server.listen(8000, () => console.log('API running on port 8000'))
